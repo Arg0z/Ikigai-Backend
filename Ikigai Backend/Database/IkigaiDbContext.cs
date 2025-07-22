10251612c54d303263e2e1ced103bb418a931c82
Arg0z <97903291+Arg0z@users.noreply.github.com>
2025-07-22 00:08:37 -0600

    Add project files.

---
+++ Ikigai Backend/Database/IkigaiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Ikigai_Backend.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Ikigai_Backend.Database
+{
+    public class IkigaiDbContext : DbContext
+    {
+        public IkigaiDbContext(DbContextOptions<IkigaiDbContext> options)
+       : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
